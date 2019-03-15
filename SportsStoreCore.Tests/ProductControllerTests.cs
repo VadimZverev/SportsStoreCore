712234525306cf90fe6cbc58452cae3145133120
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2019-03-15 20:18:44 +0400

    Unit Test: Page Model View Data

--- SportsStoreCore.Tests/ProductControllerTests.cs
+++ SportsStoreCore.Tests/ProductControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using SportsStoreCore.Controllers;
 using SportsStoreCore.Models;
+using SportsStoreCore.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -9,6 +10,36 @@ namespace SportsStoreCore.Tests
 {
     public class ProductControllerTests
     {
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"},
+                new Product { ProductID = 4, Name = "P4"},
+                new Product { ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            ProductController controller =
+                new ProductController(mock.Object) { PageSize = 3 };
+
+            // Act
+            ProductsListViewModel result =
+                controller.List(2).ViewData.Model as ProductsListViewModel;
+
+            // Assert
+            PagingInfo pagingInfo = result.PagingInfo;
+
+            Assert.Equal(2, pagingInfo.CurrentPage);
+            Assert.Equal(3, pagingInfo.ItemsPerPage);
+            Assert.Equal(5, pagingInfo.TotalItems);
+            Assert.Equal(2, pagingInfo.TotalPages);
+        }
+
         [Fact]
         public void Can_Paginate()
         {
@@ -27,11 +58,11 @@ namespace SportsStoreCore.Tests
             controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result =
-                controller.List(2).ViewData.Model as IEnumerable<Product>;
+            ProductsListViewModel result =
+                controller.List(2).ViewData.Model as ProductsListViewModel;
 
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
