50fc802e34377a39b56a8112a008ffcc36f25f83
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2019-03-17 22:54:48 +0400

    Enabling Sessions

--- SportsStoreCore/Startup.cs
+++ SportsStoreCore/Startup.cs
@@ -19,8 +19,6 @@ namespace SportsStoreCore
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -28,14 +26,16 @@ namespace SportsStoreCore
                 Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
