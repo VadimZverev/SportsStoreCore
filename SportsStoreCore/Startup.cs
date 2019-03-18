0436823f00f059ea5391f842e4d343e0dd01ac18
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2019-03-19 02:14:58 +0400

    Registering the Service

--- SportsStoreCore/Startup.cs
+++ SportsStoreCore/Startup.cs
@@ -25,6 +25,8 @@ namespace SportsStoreCore
             options.UseSqlServer(
                 Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
