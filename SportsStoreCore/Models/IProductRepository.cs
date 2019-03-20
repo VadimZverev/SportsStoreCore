99af0005cc05c08dc575196d2a3e73be5453381c
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2019-03-21 00:35:41 +0400

    Deleting Products

--- SportsStoreCore/Models/IProductRepository.cs
+++ SportsStoreCore/Models/IProductRepository.cs
@@ -7,5 +7,7 @@ namespace SportsStoreCore.Models
         IQueryable<Product> Products { get; }
 
         void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
     }
 }
